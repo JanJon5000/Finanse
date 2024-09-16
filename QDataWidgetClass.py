0b3ffa9f68e17470a2545ef4dc00d2a5e53683c9
JanJon5000 <u58462@student.wat.edu.pl>
2024-09-16 21:41:04 +0200

    working on a feature enabling the user to change single row of data in the database

--- QDataWidgetClass.py
+++ QDataWidgetClass.py
@@ -6,45 +6,72 @@ class QDataWidget(QWidget):
         super().__init__()
         self.readLayout = QHBoxLayout()
         self.currentIndex = 0
-        self.widgetList = [[], []]
         self.variables = variables
+        self.prevVariables = variables
         self.colors = colors
 
         self.populateLine()
         self.setLayout(self.readLayout)
 
     def populateLine(self):
-        for x in self.variables:
-            self.widgetList[0].append(QLabel(str(x)))
-            self.widgetList[1].append(QLineEdit(str(x)))
-            if self.variables.index(x) == 1:
-                self.widgetList[0][-1].setStyleSheet(f"color: rgb({self.colors[0]}, {self.colors[1]}, {self.colors[2]});")
-            if self.variables.index(x) == 2:
-                if x == abs(x):
-                    self.widgetList[0][-1].setStyleSheet("color: rgb(0, 255, 0);")
-                else:
-                    self.widgetList[0][-1] = QLabel('-' + str(x))
-                    self.widgetList[1][-1] = QLineEdit('-' + str(x))
-                    self.widgetList[0][-1].setStyleSheet("color: rgb(255, 0, 0)")
-        self.widgetList[0].append(QPushButton("Edycja"))
-        self.widgetList[0][-1].clicked.connect(self.changeLayout)
-        self.widgetList[1].append(QPushButton("Zapisz zmiany"))
-        self.widgetList[1][-1].clicked.connect(self.changeLayout)
-        for widg in self.widgetList[self.currentIndex]:
-            self.readLayout.addWidget(widg)
+        if self.currentIndex == 0:
+            for x in self.variables:
+                x = QLabel(str(x))
+                try: 
+                    if self.variables.index(x.text()) == 1:
+                        x.setStyleSheet(f"color: rgb({self.colors[0]}, {self.colors[1]}, {self.colors[2]});")
+                    if self.variables.index(x.text()) == 2:
+                        if float(x.text()) == abs(float(x.text())):
+                            x.setStyleSheet("color: rgb(0, 255, 0);")
+                        else:
+                            x = QLabel('-' + x.text())
+                            x.setStyleSheet("color: rgb(255, 0, 0);")
+                except:
+                    if self.variables.index(float(x.text())) == 1:
+                        x.setStyleSheet(f"color: rgb({self.colors[0]}, {self.colors[1]}, {self.colors[2]});")
+                    if self.variables.index(float(x.text())) == 2:
+                        if float(x.text()) == abs(float(x.text())):
+                            x.setStyleSheet("color: rgb(0, 255, 0);")
+                        else:
+                            x = QLabel('-' + x.text())
+                            x.setStyleSheet("color: rgb(255, 0, 0);")
+                self.readLayout.addWidget(x)
+            x = QPushButton("Edycja")
+            x.clicked.connect(self.changeLayout)
+            self.readLayout.addWidget(x)
+        elif self.currentIndex == 1:
+            for x in self.variables:
+                x = QLineEdit(str(x))
+                try:
+                    if self.variables.index(x.text()) == 2:
+                        if float(x.text()) == abs(float(x.text())):
+                            pass
+                        else:
+                            x = QLineEdit('-' + x.text())
+                except:
+                    if self.variables.index(float(x.text())) == 2:
+                        if float(x.text()) == abs(float(x.text())):
+                            pass
+                        else:
+                            x = QLineEdit('-' + x.text())
+                self.readLayout.addWidget(x)
+            x = QPushButton("Zapisz zmiany")
+            x.clicked.connect(self.changeLayout)
+            self.readLayout.addWidget(x)
 
     def changeLayout(self):
         self.clear_layout(self.layout())
         self.currentIndex = not self.currentIndex
         self.populateLine()
-        self.setLayout(self.readLayout)
         self.update()
-    
+
     def clear_layout(self, layout):
         if layout is not None:
             while layout.count():
                 child = layout.takeAt(0)
                 if child.widget() is not None:
+                    if not isinstance(child.widget(), QPushButton):
+                        print(child.widget().text())
                     child.widget().deleteLater()
                 elif child.layout() is not None:
                     self.clear_layout(child.layout())
