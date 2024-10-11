847bbcb51c54b3aa3250e752c97ba45d91baa2ae
Jan Jonca <jan@ipoczta.pl>
2024-10-11 14:06:55 +0200

    deleted useless functions in infoqidget class

--- QInfoWidget.py
+++ QInfoWidget.py
@@ -21,30 +21,9 @@ class QInfoWidget(QWidget):
                 self.accessibleLayout.addWidget(w, l.index(w)+1, columnCounter)
             columnCounter += 1
         self.setLayout(self.accessibleLayout)
-
-    def refresh(self):
-        pass
-
-    def clear_layout(self, layout):
-        if layout is not None:
-            while layout.count():
-                child = layout.takeAt(0)
-                if child.widget() is not None:
-                    child.widget().deleteLater()
-                elif child.layout() is not None:
-                    self.clear_layout(child.layout())
     
     def updateData(self, data):
         self.data = data
-        # try:
-        #     self.balanceWidget.setText(str(sum([tpl[2] for tpl in self.data])))
-        # except:
-        #     self.balanceWidget.setText('0')
-
-        # self.incomeWidgets[0].setText(str(sum([tpl[2] for tpl in self.data if tpl[2] >= 0])/len([tpl[2] for tpl in self.data if tpl[2] >= 0])))
-        # self.spendigsWidgets[0].setText(str(sum([tpl[2] for tpl in self.data if tpl[2] <= 0])/len([tpl[2] for tpl in self.data if tpl[2] >= 0])))
-        # self.incomeWidgets[1].setText(str(max([tpl[2] for tpl in self.data if tpl[2] >= 0])))
-        # self.spendigsWidgets[1].setText(str(min([tpl[2] for tpl in self.data if tpl[2] <= 0])))
         for comm in [["self.balanceWidget.setText(str(sum([tpl[2] for tpl in self.data])))", "self.balanceWidget.setText('0')"],
                      ["self.incomeWidgets[0].setText(str(sum([tpl[2] for tpl in self.data if tpl[2] >= 0])/len([tpl[2] for tpl in self.data if tpl[2] >= 0])))", "self.incomeWidgets[0].setText('0')"],
                      ["self.spendingsWidgets[0].setText(str(sum([tpl[2] for tpl in self.data if tpl[2] <= 0])/len([tpl[2] for tpl in self.data if tpl[2] >= 0])))", "self.spendingsWidgets[0].setText('0')"],
