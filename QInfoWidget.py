7fa9c0da894f02292178896349cbcc9e41f1a664
Jan Jonca <jan@ipoczta.pl>
2024-10-11 14:25:57 +0200

    lowered number of variables with unique names while maintaining infowidget class functionality. I also added more data which will be shown by the widget

--- QInfoWidget.py
+++ QInfoWidget.py
@@ -11,12 +11,14 @@ class QInfoWidget(QWidget):
 
     def populateLayout(self):
         self.balanceWidget = QLabel("test")
-        self.accessibleLayout.addWidget(self.balanceWidget, 0, 0)
-        self.infoTexts = [QLabel("srednie zyski/straty:"), QLabel("maks zyski/straty:")]
-        self.incomeWidgets = [QLabel("test0"), QLabel("test")]
-        self.spendingsWidgets = [QLabel("test0"), QLabel("test")]
+        self.dataWidgets = {
+            'infoTexts':[QLabel("srednie zyski/straty:"), QLabel("najmniejszy/największy zysk/strata w jednej transakcji"), QLabel('mediana zysków/strat'), QLabel('udzial kategorii w zyskach/stratach'), QLabel('udzial osób w zyskach/stratach')],
+            'incomeWidgets':[QLabel("test0"), QLabel("test1"), QLabel("test2"), QLabel("test3"), QLabel("test4")],
+            'spendingsWidgets':[QLabel("test0"), QLabel("test1"), QLabel("test2"), QLabel("test3"), QLabel("test4")]
+        }
+        self.accessibleLayout.addWidget(self.balanceWidget, 0, 0, 1, 3)
         columnCounter = 0
-        for l in [self.infoTexts, self.incomeWidgets, self.spendingsWidgets]:
+        for l in [self.dataWidgets['infoTexts'], self.dataWidgets['incomeWidgets'], self.dataWidgets['spendingsWidgets']]:
             for w in l:
                 self.accessibleLayout.addWidget(w, l.index(w)+1, columnCounter)
             columnCounter += 1
@@ -25,10 +27,10 @@ class QInfoWidget(QWidget):
     def updateData(self, data):
         self.data = data
         for comm in [["self.balanceWidget.setText(str(sum([tpl[2] for tpl in self.data])))", "self.balanceWidget.setText('0')"],
-                     ["self.incomeWidgets[0].setText(str(sum([tpl[2] for tpl in self.data if tpl[2] >= 0])/len([tpl[2] for tpl in self.data if tpl[2] >= 0])))", "self.incomeWidgets[0].setText('0')"],
-                     ["self.spendingsWidgets[0].setText(str(sum([tpl[2] for tpl in self.data if tpl[2] <= 0])/len([tpl[2] for tpl in self.data if tpl[2] >= 0])))", "self.spendingsWidgets[0].setText('0')"],
-                     ["self.incomeWidgets[1].setText(str(max([tpl[2] for tpl in self.data if tpl[2] >= 0])))", "self.incomeWidgets[1].setText('0')"],
-                     ["self.spendingsWidgets[1].setText(str(min([tpl[2] for tpl in self.data if tpl[2] <= 0])))", "self.spendingsWidgets[1].setText('0')"]
+                     ["self.dataWidgets['incomeWidgets'][0].setText(str(sum([tpl[2] for tpl in self.data if tpl[2] >= 0])/len([tpl[2] for tpl in self.data if tpl[2] >= 0])))", "self.dataWidgets['incomeWidgets'][0].setText('0')"],
+                     ["self.dataWidgets['spendingsWidgets'][0].setText(str(sum([tpl[2] for tpl in self.data if tpl[2] <= 0])/len([tpl[2] for tpl in self.data if tpl[2] >= 0])))", "self.self.dataWidgets['spendingsWidgets'][0].setText('0')"],
+                     ["self.dataWidgets['incomeWidgets'][1].setText(str(max([tpl[2] for tpl in self.data if tpl[2] >= 0])))", "self.dataWidgets['incomeWidgets'][1].setText('0')"],
+                     ["self.dataWidgets['spendingsWidgets'][1].setText(str(min([tpl[2] for tpl in self.data if tpl[2] <= 0])))", "self.dataWidgets['spendingsWidgets'][1].setText('0')"]
                      ]:
             try:
                 exec(comm[0])
