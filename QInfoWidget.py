755e5796bf577319f3331bb35215c452a9f8ab27
Jan Jonca <jan@ipoczta.pl>
2024-10-20 21:10:00 +0200

    more work done on the info widget - it looks a lot better now. Mostly 'in the background' stuff but it's ohnest work

--- QInfoWidget.py
+++ QInfoWidget.py
@@ -1,24 +1,48 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QLabel, QGridLayout
+from PyQt5.QtWidgets import QWidget, QLabel, QGridLayout, QVBoxLayout, QListWidget, QScrollArea, QSizePolicy
+from fundamentalClasses import SQL_SINGLE_INSTANCE
+
+class QParticipationWidget(QWidget):
+    def __init__(self, keyword, num) -> None:
+        super().__init__()
+        self.getData(keyword=keyword, num=num)
+        self.accesibleLayout = QVBoxLayout()
+        self.listComponent = QListWidget()
+        self.listComponent.setSelectionMode(QListWidget.NoSelection)
+        
+        self.scrollWidget = QScrollArea()
+        self.scrollWidget.setWidget(self.listComponent)
+        self.accesibleLayout.addWidget(self.scrollWidget)
+        self.setLayout(self.accesibleLayout)
+
+    def getData(self, keyword, num):
+        s = SQL_SINGLE_INSTANCE()
+        if keyword == 'personName':
+            s.cursor.execute('SELECT * FROM transactions, people WHERE people.idOfOther = transactions.idOfOther')
+            self.data = s.cursor.fetchall()
+        elif keyword == 'name':
+            s.cursor.execute('SELECT * FROM transactions, categories WHERE categories.idCategory = transactions.idCategory')
+            self.data = s.cursor.fetchall()
+        print(self.data)
 
 class QInfoWidget(QWidget):
     def __init__(self, data) -> None:
         super().__init__()
         self.accessibleLayout = QGridLayout()
         self.data = data
-        
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.populateLayout()
 
     def populateLayout(self):
         self.balanceWidget = QLabel("test")
         self.dataWidgets = {
-            'infoTexts':[QLabel("srednie zyski/straty:"), QLabel("największy/najmniejszy zysk/strata w jednej transakcji"), QLabel('mediana zysków/strat'), QLabel('udzial kategorii w zyskach/stratach'), QLabel('udzial osób w zyskach/stratach')],
-            'incomeWidgets':[QLabel("test0"), QLabel("test1"), QLabel("test2"), QLabel("test3"), QLabel("test4")],
-            'spendingsWidgets':[QLabel("test0"), QLabel("test1"), QLabel("test2"), QLabel("test3"), QLabel("test4")]
+            'infoTexts':[QLabel("średnia"), QLabel("max/min"), QLabel('mediana'), QLabel('udzial kategorii'), QLabel('udzial osób')],
+            'incomeWidgets':[QLabel("test0"), QLabel("test1"), QLabel("test2"), QParticipationWidget('name', 1), QParticipationWidget('personName', 1)],
+            'spendingsWidgets':[QLabel("test0"), QLabel("test1"), QLabel("test2"), QParticipationWidget('name', -1), QParticipationWidget('personName', -1)]
         }
         self.accessibleLayout.addWidget(self.balanceWidget, 0, 0, 1, 3)
         columnCounter = 0
-        for l in [self.dataWidgets['infoTexts'], self.dataWidgets['incomeWidgets'], self.dataWidgets['spendingsWidgets']]:
+        for l in [self.dataWidgets['infoTexts'], self.dataWidgets['incomeWidgets'], self.dataWidgets['spendingsWidgets']]: 
             for w in l:
                 self.accessibleLayout.addWidget(w, l.index(w)+1, columnCounter)
             columnCounter += 1
@@ -43,5 +67,16 @@ class QInfoWidget(QWidget):
     def setWidgetProperties(self):
         for w in self.dataWidgets['incomeWidgets']:
             w.setStyleSheet('color: GREEN;')
+            w.setObjectName('infotext')
+            w.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         for w in self.dataWidgets['spendingsWidgets']:
             w.setStyleSheet('color: RED;')
+            w.setObjectName('infotext')
+            w.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        for w in self.dataWidgets['infoTexts']:
+            w.setObjectName('infotext')
+            w.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        with open('styleSHEETS/info_stylesheet.qss', 'r') as file:
+            style = file.read()
+            self.setStyleSheet(style)
+        
